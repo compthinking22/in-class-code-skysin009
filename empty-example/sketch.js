88bef29c59f2ceeb99fd7b04b1782551e029830c
skysin009 <sas926@scarletmail.rutgers.edu>
2025-09-29 09:56:06 -0400

    Update sketch.js

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -3,5 +3,28 @@ function setup() {
 }
 
 function draw() {
-  // put drawing code here
+  let y= 2;
+  let x= y+5;
+  circle:mouseX (400,400);
+}
+ let y=10
+function draw() {
+  let x=y;
+  console.log(x);
+  console.log(y);
+}
+console.log(x);
+console.log(y);
+}
+
+var move= 0;
+function draw() {
+  TransformStreamDefaultController(move,100);
+  Reflect(0,100,100,150,);
+  move++;
+  if move(==350) {
+    move-=400;
+  }
+
+
 }
